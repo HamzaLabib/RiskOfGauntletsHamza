03fc0f6b549b266a3d5c0f44f809834057e7791b
HamzaLabib <86428084+HamzaLabib@users.noreply.github.com>
2021-11-17 11:30:46 -0500

    processing Commit

--- Assets/Scripts/GridMng.cs
+++ Assets/Scripts/GridMng.cs
@@ -5,6 +5,8 @@ using UnityEngine.Tilemaps;
 
 public class GridMng : MonoBehaviour
 {
+    public Grid grid;
+    public Transform bomb;
     public Tilemap distructible;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,12 @@ public class GridMng : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DeleteTile();
+    }
+
+    void DeleteTile()
+    {
+        Vector3Int position = grid.WorldToCell(bomb.position);
+        distructible.SetTile(position, null);
     }
 }
